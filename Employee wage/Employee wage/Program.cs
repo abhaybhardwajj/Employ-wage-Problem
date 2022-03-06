66808803e785cd7c72328ee4da57eb912a3a0e04
abhaybhardwajj <abhay20021998@gmail.com>
2022-03-06 22:06:54 +0530

    Calculate daily Employee Wage

--- Employee wage/Employee wage/Program.cs
+++ Employee wage/Employee wage/Program.cs
@@ -1,19 +1,22 @@
-﻿using System;
+﻿Console.WriteLine("Employee Wage Computation Problem");
+int Present = 1;  //constant
+int emp_per_rate = 20; //Constant
+int emphr = 0;
+int empwage = 0; //variable
+int Emp_per_rate = 0; //variable
 
-Console.WriteLine("Welcome to Employee Wage Computation Program.");
-
-// UC 1
-
-int is_present = 1;
-
-Random check = new Random();
-int empCheck = check.Next(0, 2);
-
-if (empCheck == is_present)
+Random random = new Random(); //computation to create the baszic object
+int EmployeeStatus = random.Next(0, 2);
+if (EmployeeStatus == Present)
 {
-    Console.WriteLine("Employee is present.");
+    Console.WriteLine("Employee is present");
+    emphr = 8;
+
 }
 else
 {
-    Console.WriteLine("Employee is absent.");
+    Console.WriteLine("Employee is Absent");
+    emphr = 0;
 }
+empwage = (Emp_per_rate * emphr);
+Console.WriteLine("Daily wage" + empwage);
