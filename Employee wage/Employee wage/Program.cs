198789c09b70033576fd8322a17e0af8405cd972
abhaybhardwajj <abhay20021998@gmail.com>
2022-03-06 22:29:36 +0530

    Add Part time Employee & Wages

--- Employee wage/Employee wage/Program.cs
+++ Employee wage/Employee wage/Program.cs
@@ -1,16 +1,29 @@
 ﻿Console.WriteLine("Employee Wage Computation Problem");
+
+//uc3
 int Present = 1;  //constant
 int emp_per_rate = 20; //Constant
 int emphr = 0;
 int empwage = 0; //variable
-int Emp_per_rate = 0; //variable
+int empfull = 1;
 
 Random random = new Random(); //computation to create the baszic object
+Random random1 = new Random();
+int Employeetime = random1.Next(0, 2);
 int EmployeeStatus = random.Next(0, 2);
 if (EmployeeStatus == Present)
 {
-    Console.WriteLine("Employee is present");
-    emphr = 8;
+
+    if (Employeetime == empfull)
+    {
+        emphr = 8;
+        Console.WriteLine("Full time Employee");
+    }
+    else
+    {
+        emphr = 4;
+        Console.WriteLine("Half time Employee");
+    }
 
 }
 else
@@ -18,5 +31,5 @@ else
     Console.WriteLine("Employee is Absent");
     emphr = 0;
 }
-empwage = (Emp_per_rate * emphr);
-Console.WriteLine("Daily wage" + empwage);
+empwage = (emp_per_rate * emphr);
+Console.WriteLine("Daily wage " + empwage);
